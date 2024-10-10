4e1a6759f6f07e94322d6d5e046b3b076c0ab4fc
alp <dognalp.erden@tum.de>
2024-10-10 17:48:50 +0200

    now able to fetch event data for active match automatically

--- notifier.py
+++ notifier.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from datetime import datetime
 
 # TODO: Import update interval and selected teams
@@ -16,24 +17,42 @@ class MackolikRunner(object):
     def __init__(self):
         self.s = requests.Session()
         self.all_matches_url = "https://www.mackolik.com/perform/p0/ajax/components/competition/livescores/json?"
+        self.match_event_url = "https://www.mackolik.com/ajax/football/key-events?"
 #...HEADERS...#######################################################################################################################################
         self.general_header = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Connection': 'keep-alive',
-            'DNT': '1',
-            'Host': 'www.mackolik.com',
-            'Priority': 'u=0, i',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Sec-GPC': '1',
-            'TE': 'trailers',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
-        }
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive',
+                'DNT': '1',
+                'Host': 'www.mackolik.com',
+                'Priority': 'u=0, i',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Sec-GPC': '1',
+                'TE': 'trailers',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
+                }
+        self.match_event_header = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive',
+                'DNT': '1',
+                'Host': 'www.mackolik.com',
+                'Priority': 'u=0, i',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Sec-GPC': '1',
+                'TE': 'trailers',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
+                }
 #...FUNCTIONS...#####################################################################################################################################
 
 
@@ -49,11 +68,45 @@ class MackolikRunner(object):
         today = datetime.today().strftime('%Y-%m-%d')
         parameters = "sports[]=Soccer&matchDate="+today
         all_matches = self.getResponse( self.all_matches_url , parameters , self.general_header )
-        print(all_matches)
+        if all_matches:
+            all_matches = all_matches.get('data').get('matches')
+            #print(all_matches)
+            return all_matches
+        else:
+            return None
+
+    def getEvent(self, all_matches, team_name, event_num):
+        if all_matches:
+            for match in all_matches:
+                current_match = all_matches.get(match)
+                if current_match.get("state") == "live":
+                    home_team = current_match.get("homeTeam").get("name")
+                    away_team = current_match.get("awayTeam").get("name")
+                    if (home_team == team_name) or (away_team == team_name):
+                        #print(match)
+                        parameters = "ajaxViewName=events&matchId="+match
+                        match_events = self.getResponse(self.match_event_url, parameters , self.match_event_header)
+                        #print(match_events)
+                        if match_events and len(match_events.get("data").get("keyEvents"))>event_num:
+                            #TODO set different behaviour for different event types
+                            print(str(match_events.get("data").get("keyEvents")[event_num])+"\n")
+                            return True
+        return False
+                        
+
 
 def main():
+    teams_list = [("Portekiz U20", 0), ("Osmaniyespor FK",0)]
     fetcher = MackolikRunner()
-    fetcher.getAllMatches()
+    while(1):
+        all_matches = fetcher.getAllMatches()
+        for i in range(len(teams_list)):
+            #TODO check whether the event was displayed or not before calling the method
+            team_name, event_num = teams_list[i]
+            event_ready = fetcher.getEvent(all_matches, team_name, event_num)
+            if event_ready:
+                event_num += 1
+                teams_list[i] = (team_name, event_num)
 
 
 
